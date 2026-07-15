25832f694dae5486f0f63566856eb893f5f74109
Justin Kwan <jnpkwan@users.noreply.github.com>
2026-07-15 21:47:21 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,13 @@
 const CACHE_NAME = 'ahbc-inventory-v' + Date.now();
 const ASSETS = [
   './inventory-count.html',
-  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&family=Nunito+Sans:wght@400;500;600;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+  './manifest.json',
+  './logo.png',
+  './tree-mark.png',
+  './icon-192.png',
+  'https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800;900&family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js',
+  'https://unpkg.com/@zxing/library@0.19.1/umd/index.min.js'
 ];
 
 // Install: cache assets and activate immediately
